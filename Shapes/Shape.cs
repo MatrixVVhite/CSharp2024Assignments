38aee47227690ce06d33b20fc4206c5637391e95
NeoThePotato <neorayb@gmail.com>
2024-01-16 10:01:26 +0200

    Implemented abstract "Shape" class.

---
+++ Shapes/Shape.cs
@@ -0,0 +1,24 @@
+﻿namespace BloodyShapes.Shapes
+{
+	abstract class Shape
+	{
+		public float PositionX { get; protected set; }
+		public float PositionY { get; protected set; }
+		public float Width { get; protected set; }
+		public float Height { get; protected set; }
+
+		protected Shape(float width = 1, float height = 1, float posX = 0, float posY = 0)
+		{
+			PositionX = posX;
+			PositionY = posY;
+			Width = width;
+			Height = height;
+		}
+
+		public abstract float Area();
+
+		public abstract float Perimeter();
+
+		public virtual void Draw() => Console.WriteLine(this);
+	}
+}
