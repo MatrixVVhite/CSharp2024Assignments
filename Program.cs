a3a383be8f93ba5ccec43d5f7f05e36b43ff890f
NeoThePotato <neorayb@gmail.com>
2024-02-27 22:14:39 +0200

    Finished exercise A.

--- Program.cs
+++ Program.cs
@@ -6,11 +6,37 @@ public class Program
 {
 	public static void Main()
 	{
-		ObservableLimitedList<string> list = new(s => IsPalindrome(s));
+		// Exercise A - Delegates
+		DelegatesExercise();
+		//Some funny stuff I did for no reason
+		DelegatesExerciseBonus();
+	}
+
+	private static void DelegatesExercise()
+	{
+		Console.WriteLine("--- Exercise A - Delegates ---\nPress any key to run");
+		Utility.BlockUntilKeyDown();
+		ObservableLimitedList<string> list = new(ContainsTheLetterS);
+		list.ListChanged += s => Console.WriteLine(s);
+		string[] str = { "Lemonade", "Shield", "Calamity", "Pirate", "Season", "Door", "Lock", "Possessionlessnesses", "Event", "Delegate"};
+		foreach (var s in str)
+			list.Add(s);
+	}
+
+	private static void DelegatesExerciseBonus()
+	{
+		Console.WriteLine("--- Same as previous, but I used an IsPalindrome condition, just because ---\nPress any key to run");
+		Utility.BlockUntilKeyDown();
+		ObservableLimitedList<string> list = new(IsPalindrome);
 		list.ListChanged += s => Console.WriteLine(s);
 		var str = "Wait until they see the Hannah Montana episode with a racecar The kayak cop with a radar sees a nun in a Civic Those were some next level stats".Split(' ');
 		foreach (var s in str)
 			list.Add(s);
+	}
+
+	private static bool ContainsTheLetterS(string s)
+	{
+		return s.ToLower().Contains('s');
 	}
 
 	private static bool IsPalindrome(string s)
