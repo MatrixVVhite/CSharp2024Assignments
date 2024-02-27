7f022c8804fbf5865d7485077dcde7ab78fd5500
NeoThePotato <neorayb@gmail.com>
2024-02-27 13:43:43 +0200

    Created LINQ project.

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿// ---- C# II (Dor Ben Dor) ----
+//       Nehorai Buader
+// ----------------------------
+// Assignment 10 - LINQ (H.A.)
+public class Program
+{
+	public static void Main()
+	{
+
+	}
+}
