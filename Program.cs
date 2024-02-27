494d67f0a732472aab4f64f4bc82c396cff0c76d
NeoThePotato <neorayb@gmail.com>
2024-02-27 13:43:43 +0200

    Created Tree Data Structure project.

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿// ---- C# II (Dor Ben Dor) ----
+//       Nehorai Buader
+// ----------------------------
+// Assignment 9 - The Tree Data Structure (E.C.)
+public class Program
+{
+	public static void Main()
+	{
+
+	}
+}
