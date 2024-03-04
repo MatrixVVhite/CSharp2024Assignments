178d32b6d8e32076ea537a4aae0af168f5bde858
NeoThePotato <neorayb@gmail.com>
2024-03-04 19:22:01 +0200

    Made Dice a generic class.

--- GenericTypesAndIComparable/Random/Dice.cs
+++ GenericTypesAndIComparable/Random/Dice.cs
@@ -1,36 +1,24 @@
 ﻿namespace Berzerkers.Random
 {
-	public readonly struct Dice : IRandomProvider<int>, IEquatable<Dice>
+	public class Dice : Dice<int>
 	{
-		private readonly Uniform _die;
-
-		public byte Scalar { get; init; }
-		public byte BaseDie => (byte)_die.Max;
 		public short Modifier { get; init; }
-		public int Min => Modifier + Scalar;
-		public int Max => Modifier + Scalar * BaseDie;
 
-		public Dice(byte scalar = 1, byte baseDie = 6, short modifier = 0)
+		public Dice(byte scalar = 1, byte baseDie = 6, short modifier = 0) : base(scalar, Enumerable.Range(1, baseDie).ToArray())
 		{
-			Scalar = scalar;
-			_die = new(1, baseDie);
 			Modifier = modifier;
 		}
 
 		public int GetRandom() => Roll();
 
-		public readonly int Roll()
+		public new int Roll()
 		{
-			int retVal = Modifier;
-			for (int d = 0; d < Scalar; d++)
-				retVal += RollSingle();
-			return retVal;
+			int[] result = base.Roll();
+			return result.Sum() + Modifier;
 		}
 
-		private readonly int RollSingle() => 1 + IRandomProvider<int>.RANDOM.Next(BaseDie);
-
 		#region OVERRIDES
-		public override readonly string ToString() => $"{Scalar}d{BaseDie}{(Modifier >= 0 ? '+' : string.Empty)}{Modifier}";
+		public override string ToString() => $"{Scalar}d{BaseDie}{(Modifier >= 0 ? '+' : string.Empty)}{Modifier}";
 
 		public override bool Equals(object? obj) => obj is Dice dice && Equals(dice);
 
@@ -41,7 +29,7 @@
 				   Modifier == other.Modifier;
 		}
 
-		public override readonly int GetHashCode() => (Scalar, BaseDie, Modifier).GetHashCode();
+		public override int GetHashCode() => (Scalar, BaseDie, Modifier).GetHashCode();
 
 		public static bool operator ==(Dice left, Dice right) => left.Equals(right);
 
---
+++ GenericTypesAndIComparable/Random/DiceGeneric.cs
@@ -0,0 +1,24 @@
+﻿namespace Berzerkers.Random
+{
+	public class Dice<T> where T : IComparable<T>
+	{
+		public int Scalar { get; init; }
+		public T[] BaseDie { get; init;}
+
+		public Dice(int scalar, T[] baseDie)
+		{
+			Scalar = scalar;
+			BaseDie = baseDie;
+		}
+
+		public virtual T[] Roll()
+		{
+			T[] result = new T[Scalar];
+			for (int d = 0; d < Scalar; d++)
+				result[d] = RollSingle();
+			return result;
+		}
+
+		private T RollSingle() => BaseDie.GetRandom();
+	}
+}
