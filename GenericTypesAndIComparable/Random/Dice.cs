1f867fa3941b2bb4a0ab45d2dffc11daccb33c02
NeoThePotato <neorayb@gmail.com>
2024-03-10 16:28:40 +0200

    Implemented working Dice.

--- GenericTypesAndIComparable/Random/Dice.cs
+++ GenericTypesAndIComparable/Random/Dice.cs
@@ -1,39 +1,34 @@
-﻿namespace Berzerkers.Random
+﻿public class Dice : Dice<int>
 {
-	public class Dice : Dice<int>
+	public short Modifier { get; init; }
+
+	public Dice(byte scalar = 1, byte baseDie = 6, short modifier = 0) : base(scalar, Enumerable.Range(1, baseDie).ToArray())
 	{
-		public short Modifier { get; init; }
-
-		public Dice(byte scalar = 1, byte baseDie = 6, short modifier = 0) : base(scalar, Enumerable.Range(1, baseDie).ToArray())
-		{
-			Modifier = modifier;
-		}
-
-		public int GetRandom() => Roll();
-
-		public new int Roll()
-		{
-			int[] result = base.Roll();
-			return result.Sum() + Modifier;
-		}
-
-		#region OVERRIDES
-		public override string ToString() => $"{Scalar}d{BaseDie}{(Modifier >= 0 ? '+' : string.Empty)}{Modifier}";
-
-		public override bool Equals(object? obj) => obj is Dice dice && Equals(dice);
-
-		public bool Equals(Dice other)
-		{
-			return Scalar == other.Scalar &&
-				   BaseDie == other.BaseDie &&
-				   Modifier == other.Modifier;
-		}
-
-		public override int GetHashCode() => (Scalar, BaseDie, Modifier).GetHashCode();
-
-		public static bool operator ==(Dice left, Dice right) => left.Equals(right);
-
-		public static bool operator !=(Dice left, Dice right) => !(left == right);
-		#endregion
+		Modifier = modifier;
 	}
+
+	public new int Roll()
+	{
+		int[] result = base.Roll();
+		return result.Sum() + Modifier;
+	}
+
+	#region OVERRIDES
+	public override string ToString() => $"{Scalar}d{BaseDie.Last()}{(Modifier >= 0 ? '+' : string.Empty)}{Modifier}";
+
+	public override bool Equals(object? obj) => obj is Dice dice && Equals(dice);
+
+	public bool Equals(Dice other)
+	{
+		return Scalar == other.Scalar &&
+				BaseDie == other.BaseDie &&
+				Modifier == other.Modifier;
+	}
+
+	public override int GetHashCode() => (Scalar, BaseDie, Modifier).GetHashCode();
+
+	public static bool operator ==(Dice left, Dice right) => left.Equals(right);
+
+	public static bool operator !=(Dice left, Dice right) => !(left == right);
+	#endregion
 }
