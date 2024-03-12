9d16944db8835b27183d74986b09a19e6d29ea49
NeoThePotato <neorayb@gmail.com>
2024-03-12 03:10:42 +0200

    Moved the classes to proper namespace.

---
+++ GenericTypesAndIComparable/Structs/Deck.cs
@@ -0,0 +1,52 @@
+﻿namespace GenericTypesAndIComparable.Structs
+{
+    public class Deck<T> where T : struct, IComparable<T>
+    {
+        private List<T> Active { get; set; }
+        private List<T> Discarded { get; set; }
+        public int Size => Active.Capacity;
+        public int Remaining => Active.Count;
+
+        public Deck(ICollection<T> values)
+        {
+            Active = values.ToList();
+            Discarded = new List<T>(Size);
+        }
+
+        public bool TryDraw(out T drawn)
+        {
+            if (Remaining > 0)
+            {
+                drawn = Draw();
+                Discarded.Add(drawn);
+                return true;
+            }
+            drawn = default;
+            return false;
+        }
+
+        public void Shuffle() => Active.Shuffle();
+
+        public void ReShuffle()
+        {
+            MoveDiscardedToActive();
+            Shuffle();
+        }
+
+        public T Peek() => Active.Last();
+
+        private void MoveDiscardedToActive()
+        {
+            foreach (var item in Discarded)
+                Active.Add(item);
+            Discarded.Clear();
+        }
+
+        private T Draw()
+        {
+            T drawn = Active.Last();
+            Active.RemoveAt(Remaining - 1);
+            return drawn;
+        }
+    }
+}
---
+++ GenericTypesAndIComparable/Structs/DiceGeneric.cs
@@ -0,0 +1,24 @@
+﻿namespace GenericTypesAndIComparable.Structs
+{
+    public class Dice<T> where T : IComparable<T>
+    {
+        public int Scalar { get; init; }
+        public T[] BaseDie { get; init; }
+
+        public Dice(int scalar, ICollection<T> baseDie)
+        {
+            Scalar = scalar;
+            BaseDie = baseDie.ToArray();
+        }
+
+        public virtual T[] Roll()
+        {
+            T[] result = new T[Scalar];
+            for (int d = 0; d < Scalar; d++)
+                result[d] = RollSingle();
+            return result;
+        }
+
+        private T RollSingle() => BaseDie.GetRandom();
+    }
+}
