3486cb6175f40d219127157150df65f54df5c736
NeoThePotato <neorayb@gmail.com>
2024-03-12 04:00:24 +0200

    Tested the RandomFighter algorithm.

--- GenericTypesAndIComparable/Program.cs
+++ GenericTypesAndIComparable/Program.cs
@@ -2,10 +2,26 @@
 //       Nehorai Buader
 // ----------------------------
 // Assignment 08 - Generic Types and IComparable
-public static class Program
-{
-	public static void Main()
-	{
+using GenericTypesAndIComparable.Structs;
 
+namespace GenericTypesAndIComparable
+{
+    public static class Program
+	{
+		public static void Main()
+		{
+			RandomFighter();
+		}
+
+		private static void RandomFighter()
+		{
+			var arr = new int[40];
+			for (int i = 0; i < arr.Length; i++)
+				arr[i] = Random.Shared.Next(20);
+			var deck = new Deck<int>(arr);
+			var die = new Dice(1, 20);
+			var results = GenericTypesAndIComparable.RandomFighter.Fight(deck, die);
+			Console.WriteLine(results);
+		}
 	}
 }
